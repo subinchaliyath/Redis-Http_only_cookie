48dcdd4cdaf43372b3346226748a8e37c9298701
subin <subinchaliyath@gmail.com>
2021-05-25 23:29:44 +0530

    jwt sign verify methods added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const redis = require("redis");
 const cookie = require("cookie");
 const jwt = require("jsonwebtoken");
-var cookieParser = require('cookie-parser');
+var cookieParser = require("cookie-parser");
 
 const PORT = process.env.PORT || 5000;
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
@@ -24,18 +24,16 @@ async function getRepos(req, res, next) {
   try {
     console.log("Fetching Data...");
 
-    const { username } = req.params;
-
-    const response = await fetch(`https://api.github.com/users/${username}`);
+    const response = await fetch(`https://api.github.com/users/${req.user}`);
 
     const data = await response.json();
 
     const repos = data.public_repos;
 
     // Set data to Redis
-    client.setex(username, 3600, repos);
+    client.setex(req.user, 3600, repos);
 
-    res.send(setResponse(username, repos));
+    res.send(setResponse(req.user, repos));
   } catch (err) {
     console.error(err);
     res.status(500);
@@ -44,7 +42,7 @@ async function getRepos(req, res, next) {
 
 // Cache middleware
 function cache(req, res, next) {
-  const { username } = req.params;
+  const username = req.user;
 
   client.get(username, (err, data) => {
     if (err) throw err;
@@ -56,21 +54,36 @@ function cache(req, res, next) {
     }
   });
 }
-function checkjwt(req,res,next){
-   if(req.cookies['token']===token) {
-       next();
-   }else{
-       res.json("no cookie found")
-   }
+
+// jwt verification
+function checkjwt(req, res, next) {
+  if (req.cookies["token"]) {
+    jwt.verify(req.cookies["token"], "shhhhh", (err, { username }) => {
+      if (err) return res.sendStatus(403);
+      req.user = username;
+      next();
+    });
+  } else {
+    res.json("no cookie found");
+  }
 }
-var token = "generatetoken";
+
+  
+function firstScreen() {
+  return `<h2>Login with /login/:username</h2>
+            <h3>Get number of repos by /repos/:username</h3>
+           `;
+}
+
+
 function login(req, res) {
+  const token = jwt.sign({ username: req.params.username }, "shhhhh");
   res.setHeader(
     "Set-Cookie",
     cookie.serialize("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
-      maxAge: 60 * 60 * 24 * 7, // 1 week
+      maxAge: 60 * 60 * 24, // 1 day
       sameSite: "strict",
       path: "/",
     })
@@ -78,9 +91,16 @@ function login(req, res) {
   res.json("cookie set");
 }
 
-app.get("/repos/:username", checkjwt,cache, getRepos);
-app.get("/", (req, res) => res.json("running"));
-app.get("/login", login);
+
+// to check the server
+app.get("/", (req, res) => res.send(firstScreen()));
+
+app.get("/login/:username", login);
+
+// this is a protected route
+// redis cache implemented
+app.get("/repos/:username", checkjwt, cache, getRepos);
+
 
 app.listen(5000, () => {
   console.log(`App listening on port ${PORT}`);
